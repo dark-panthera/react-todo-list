ee7fcd73c871d4381ca540a0301e80e93d9f6d73
panthera-dark <panthera.dark@hotmail.com>
2020-07-12 14:47:22 +0200

    Updated TodoInput

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+// https://www.fontawesomecheatsheet.com/font-awesome-cheatsheet-5x/
 
 class TodoItem extends Component {
     render() {
         return (
             <div>
-                hello from todo item
-            </div>
+                hello from todo item <i class="fas fa-moon"></i> <i class="fab fa-asymmetrik"></i> <i class="fas fa-eye-dropper"></i>
+            
+                <i class="fas fa-feather-alt"></i>    </div>
         )
     }
 }
