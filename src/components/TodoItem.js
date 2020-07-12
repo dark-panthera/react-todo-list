08907a3ff58048a9009317bda9c2e510f05c219e
panthera-dark <panthera.dark@hotmail.com>
2020-07-12 16:02:15 +0200

    Changed Bootstrap v4.3.1

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,15 +3,28 @@ import React, { Component } from 'react'
 
 class TodoItem extends Component {
     render() {
+        const { title, handleDelete, handleEdit } = this.props;
+
         return (
-            <div>
-                hello from todo item <i className="fas fa-moon"></i> 
-                <i className="fab fa-asymmetrik"></i> 
-                <i className="fas fa-eye-dropper"></i>
-                <i className="fas fa-feather-alt"></i>
-            </div>
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                <h6>{title}</h6>
+                <div className="todo-icon">
+                    <span className="mx-2 text-success" onClick={handleEdit}>
+                        <i className="fas fa-pen"></i> 
+                    </span>
+                    <span className="mx-2 text-danger" onClick={handleDelete}>
+                        <i className="fas fa-trash"></i> 
+                    </span>
+                </div>
+            </li>
         )
     }
 }
 
 export default TodoItem;
+/*
+hello from todo item <i className="fas fa-moon"></i> 
+<i className="fab fa-asymmetrik"></i> 
+<i className="fas fa-eye-dropper"></i>
+<i className="fas fa-feather-alt"></i>
+*/
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react'
-import Item from './TodoItem';
+import React, { Component } from "react";
+import Item from "./TodoItem";
+import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
     render() {
+        const { items, clearList, handleDelete, handleEdit } = this.props;
+
         return (
-            <section>
-                <h2>todo list</h2>
-                <Item />
-            </section>
-        )
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">todo list</h3>
+                {items.map((item) => {
+                    return (
+                        <TodoItem
+                            key={item.id}
+                            title={item.title}
+                            handleEdit={() => handleEdit(item.id)}
+                            handleDelete={() => handleDelete(item.id)}
+                        ></TodoItem>
+                    );
+                })}
+                <button
+                    type="button"
+                    className="btn btn-danger btn-block text-uppercase mt-5"
+                    onClick={clearList}
+                >
+                    Clear
+                </button>
+            </ul>
+        );
     }
 }
 
 export default TodoList;
+/*
+<section>
+    <h2>todo list</h2>
+    <Item>hello from todo item</Item>
+</section>
+*/
